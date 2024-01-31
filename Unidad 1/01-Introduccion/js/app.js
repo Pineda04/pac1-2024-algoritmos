45d82a21926c5e2e8640e2494e924ec155fd0bb2
Pineda04 <carlosovidiopineda8@gmail.com>
2024-01-30 20:52:15 -0600

    first commit

---
+++ Unidad 1/01-Introduccion/js/app.js
@@ -0,0 +1,8 @@
+const  nombre = prompt('Como te llamas? ')
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavaScript`
+
+/*
+    Comentario mutilinea
+*/
+
+//Comentario de na linea
